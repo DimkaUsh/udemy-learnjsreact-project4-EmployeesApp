6755b6e0205857c4a7fa5890bddd690a0db151ca
Dmitrii Ushakov <daushakov89@mail.ru>
2022-03-31 13:56:35 +0800

    feat: add search-panel

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className='form-control search-input'
+            placeHolder='Найти сотрудника' />
+    )
+}
+
+export default SearchPanel
